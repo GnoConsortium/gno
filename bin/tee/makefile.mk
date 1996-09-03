8b04cea89323e71b64644736603994e837e3521a
gdr <gdr>
1996-09-03 03:56:08 +0000

    - Added -b flag. - Added *.rez file. - Makefile now uses install(1) for installation.

--- bin/tee/makefile.mk
+++ bin/tee/makefile.mk
@@ -1,11 +1,29 @@
-# CFLAGS	= -w -i -G25 -v -DCHECK_STACK=1
+#
+# This makefile is intended for use with dmake(1)
+#
+# $Id: makefile.mk,v 1.2 1996/09/03 03:56:06 gdr Exp $
+#
+
+INSTALL	= /usr/bin/install
+BINDIR	= /bin
+MANDIR	= /man/man1
+
+DEFINES	= -D_POSIX_SOURCE
+
+# CFLAGS	= -w -i -G25 -v -DCHECK_STACK=1 $(DEFINES)
 # LDFLAGS	= -l/usr/lib/gnulib -l/usr/lib/stack
 
-CFLAGS	= -w -i -O -s768
+CFLAGS	= -w -i -O -s768 $(DEFINES)
 LDFLAGS	= -l/usr/lib/gnulib -s768
 
-all: tee
+tee: tee.o tee.r
+	$(CC) $(LDFLAGS) tee.o $(LDLIBS) -o $@
+	copyfork tee.r tee -r
 
 install:
-	cp -f tee /bin
-	cp -f tee.1 /usr/man/man1
+	$(INSTALL) -m755 -obin -gsys -d $(BINDIR) $(MANDIR)
+	$(INSTALL) -m755 -obin -gsys tee   $(BINDIR)
+	$(INSTALL) -m644 -obin -gsys tee.1 $(MANDIR)
+
+clean clobber:
+	$(RM) tee.r tee.o tee.root
--- bin/tee/tee.c
+++ bin/tee/tee.c
@@ -1,13 +1,16 @@
 /*
  * tee - send a copy of stdin to a file as well as stdout.
  *
- * Version 1.1 by Devin Reade <gdr@myrias.ab.ca>
+ * Version 1.1 and later by Devin Reade <gdr@myrias.ab.ca>
  *
  * tee originally appeared with Gno v1.x, but was fully buffered.
  * This is a complete re-write which uses full buffering for the
  * output file, but _no_ buffering on stdin and stdout.
+ *
+ * $Id: tee.c,v 1.2 1996/09/03 03:56:07 gdr Exp $
  */
 
+#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -16,125 +19,170 @@
 #include "getopt.h"
 
 #ifdef CHECK_STACK
-	void begin_stack_check(void);
-	int end_stack_check(void);
-#  define STACKSTART begin_stack_check()
-#  define STACKEND(n) { \
-		fprintf(stderr,"stack usage: %d bytes\n",end_stack_check()); \
-      return n; \
-   }
+void begin_stack_check(void);
+int end_stack_check(void);
+
+#define STACKSTART begin_stack_check()
+#define STACKEND(n) { \
+  fprintf(stderr,"stack usage: %d bytes\n",end_stack_check()); \
+  return n; \
+}
 #else
-#	define STACKSTART
-#  define STACKEND(n) return n
+#define STACKSTART
+#define STACKEND(n) return n
 #endif
 
 #define BUFFERSIZE 512
 #define USAGE { \
-	fprintf(stderr,"%s %s\nUsage:\t%s %s\n",argv[0],versionstr,argv[0], \
-		     usagestr); \
-   STACKEND(-1); \
+  fprintf(stderr,"%s %s\nUsage:\t%s %s\n",argv[0],versionstr,argv[0], \
+          usagestr); \
+  STACKEND(-1); \
 }
 
-char *versionstr="version 1.1 by Devin Reade";
-char *usagestr="[ -ai ] filename\n\
+char *versionstr = "version 1.2 by Devin Reade";
+char *usagestr = "[ -abiV ] filename\n\
 \t-a\tappend to filename\n\
 \t-i\tignore SIGINT\n";
 
 char buf2[BUFFERSIZE];
 
-int main (int argc, char **argv) {
-	int c;
-   char *mode, buf;
-   FILE *fp, *fp2;
-   extern int optind;
+int
+main(int argc, char **argv)
+{
+  int c, b_flag;
+  char *mode, buf;
+  FILE *fp, *fp2;
+  extern int optind;
 
-   /* initialization */
-	STACKSTART;
-   mode = "w+";
+  /*
+   * initialization 
+   */
+  STACKSTART;
+  mode = "w+";
+  b_flag = 0;
 
-   /* parse the command line */
-   while ((c = getopt(argc, argv, "aiV")) != EOF)
-	   switch (c) {
-      case 'a':
-	      /* append to instead of truncate output file*/
-	      mode = "a+";
-         break;
-      case 'i':
-	      /* ignore SIGINT */
-    		signal(SIGINT,SIG_IGN);
-         break;
-      case 'V':	/*FALLTHROUGH*/
-      default:
-	      USAGE;
-         /*NOTREACHED*/
+  /*
+   * parse the command line 
+   */
+  while ((c = getopt(argc, argv, "abiV")) != EOF) {
+    switch (c) {
+    case 'a':
+      /* append to instead of truncate output file */
+      mode = "a+";
+      break;
+
+    case 'b':
+      /* do line buffering */
+      b_flag++;
+      break;
+
+    case 'i':
+      /* ignore SIGINT */
+      signal(SIGINT, SIG_IGN);
+      break;
+
+    case 'V':
+      /* FALLTHROUGH */
+    default:
+      USAGE;
+      /* NOTREACHED */
+    }
+  }
+  if ((argc - optind) < 1) {
+    USAGE;
+  }
+  /*
+   * open the output file 
+   */
+  if ((fp = fopen(argv[optind], mode)) == NULL) {
+    perror("opening master file");
+    STACKEND(1);
+  }
+  /*
+   * loop until done with the first file 
+   */
+  if (b_flag) {
+    /* line buffering */
+    int done = 0;
+
+    while (!done) {
+      c = fread(buf2, sizeof(char), BUFFERSIZE, stdin);
+
+      if (c == 0) {
+	if (ferror(stdin)) {
+	  fclose(fp);
+	  STACKEND(1);
+	  /* NOTREACHED */
+	}
+	done = 1;
       }
-   if ((argc - optind) < 1) USAGE;
+      fwrite(buf2, sizeof(char), c, stdout);
+      fwrite(buf2, sizeof(char), c, fp);
+    }
+  } else {
+    /* no buffering */
+    int done = 0;
 
-   /* open the output file */
-   if ((fp = fopen(argv[optind],mode)) == NULL) {
-	   perror("opening master file");
-      STACKEND(1);
-   }
-
-   /* loop until done with the first file */
-	for(;;) {
-	   int done = 0;
-	   switch (read(STDIN_FILENO,&buf,1)) {
+    while (!done) {
+      switch (read(STDIN_FILENO, &buf, 1)) {
       case -1:
-      	fclose(fp);
-         STACKEND(1);
-         /*NOTREACHED*/
+	fclose(fp);
+	STACKEND(1);
+	/* NOTREACHED */
       case 0:
-         done=1;
-         break;
+	done = 1;
+	break;
       default:
-	      write(STDOUT_FILENO,&buf,1);
-         fputc(buf,fp);
-         break;
+	write(STDOUT_FILENO, &buf, 1);
+	fputc(buf, fp);
+	break;
       }
-      if (done) break;
-   }
+    }
+  }
 
-   /* make additional copies if necessary */
-   optind++;
-   if (argc<=optind) {
-		fclose(fp);
-   	STACKEND(0);
-   }
-	while (argc>optind) {
-	   size_t count;
+  /*
+   * make additional copies if necessary 
+   */
+  optind++;
+  if (argc <= optind) {
+    fclose(fp);
+    STACKEND(0);
+  }
+  while (argc > optind) {
+    size_t count;
 
-      /* rewind the master file */
-      rewind(fp);
+    /* rewind the master file */
+    rewind(fp);
 
-      /* open the new file */
-   	if ((fp2 = fopen(argv[optind],mode)) == NULL) {
-	   	perror("opening duplicate file");
-         fclose(fp);
-      	STACKEND(1);
-   	}
+    /* open the new file */
+    if ((fp2 = fopen(argv[optind], mode)) == NULL) {
+      perror("opening duplicate file");
+      fclose(fp);
+      STACKEND(1);
+    }
+    /* make the copy */
+    while (!feof(fp) && !(ferror(fp))) {
+      count = fread(buf2, sizeof(char), BUFFERSIZE, fp);
 
-      /* make the copy */
-      while (!feof(fp) && !(ferror(fp))) {
-      	count = fread(buf2,sizeof(char),BUFFERSIZE,fp);
-         if (count>0) {
-	         fwrite(buf2,sizeof(char),count,fp2);
-            if (ferror(fp2)) {
-	            perror("writing duplicate file");
-	            fclose(fp);
-	            fclose(fp2);
-	            STACKEND(1);
-            }
-         }
+      if (count > 0) {
+	fwrite(buf2, sizeof(char), count, fp2);
+
+	if (ferror(fp2)) {
+	  perror("writing duplicate file");
+	  fclose(fp);
+	  fclose(fp2);
+	  STACKEND(1);
+	}
       }
-      fclose(fp2);
-      if (ferror(fp)) {
-	      perror("reading master file");
-         fclose(fp);
-         STACKEND(1);
-      }
-      optind++;
-   }
-   fclose(fp);
-   STACKEND(0);
+    }
+    fclose(fp2);
+    if (ferror(fp)) {
+      perror("reading master file");
+      fclose(fp);
+      STACKEND(1);
+    }
+    optind++;
+  }
+  fclose(fp);
+  STACKEND(0);
 }
