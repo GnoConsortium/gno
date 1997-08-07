397a3206ab0dbb1892b92d721086237e966c76bc
gdr <gdr>
1997-08-07 05:17:22 +0000

    initial checkin

---
+++ usr.bin/false/false.c
@@ -0,0 +1,12 @@
+/*
+ * gno/usr.bin/false.c
+ *
+ * Devin Reade, 1997.  (Are you impressed, yet?)
+ *
+ * $Id: false.c,v 1.1 1997/08/07 05:17:22 gdr Exp $
+ */
+
+int
+main(int argc, char **argv) {
+  return -1;
+}
---
+++ usr.bin/true/true.c
@@ -0,0 +1,12 @@
+/*
+ * gno/usr.bin/true.c
+ *
+ * Devin Reade, 1997.  (Are you impressed, yet?)
+ *
+ * $Id: true.c,v 1.1 1997/08/07 05:16:58 gdr Exp $
+ */
+
+int
+main(int argc, char **argv) {
+  return 0;
+}
