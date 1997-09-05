4fead8bdd039f55b421d05344be42a006ea78294
gdr <gdr>
1997-09-05 06:48:12 +0000

    minor comment changes

--- lib/libc/rules.mk
+++ lib/libc/rules.mk
@@ -1,15 +1,20 @@
 #
-# Default rules for libc/gno (override of /usr/local/lib/startup.mk)
+# gno/lib/libc/rules.mk
+#
+# Default rules for libc (override of /usr/local/lib/startup.mk).
+#
 # Devin Reade, 1997
 #
-# $Id: rules.mk,v 1.1 1997/02/28 05:12:39 gdr Exp $
+# $Id: rules.mk,v 1.2 1997/09/05 06:48:12 gdr Exp $
 #
 
 %.o: %.c
 	$(CC) -o $@ -c $(__OFLAG) $(CFLAGS) $<
-#	$(MAKELIB) -l $(LIBC) $(MAKELIBFLAGS) $@
 
 %.o: %.asm
 	$(AS) -o $@ -c $(__OFLAG) $(ASFLAGS) $<
 	@$(RM) $*.root
-#	$(MAKELIB) -l $(LIBC) $(MAKELIBFLAGS) $@
+
+#../libc: $(OBJS)
+#	$(MAKELIB) $(MAKELIBFLAGS) -l $@ $(OBJS)
+              
