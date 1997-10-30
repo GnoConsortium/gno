51ae0e883d48b21be268be9388468c983207c037
gdr <gdr>
1997-10-30 02:13:59 +0000

    initial checkin.  This is provided for compatibility with old BSD sources

---
+++ include/sys/file.h
@@ -0,0 +1,16 @@
+/*
+ * Too bad we don't have symlinks for GNO ...
+ *
+ * $Id: file.h,v 1.1 1997/10/30 02:13:59 gdr Exp $
+ */
+
+#ifndef _SYS_FILE_H_
+#define _SYS_FILE_H_
+
+/* <sys/file.h> is the old BSD name for <sys/fcntl.h> */
+
+#ifndef _SYS_FCNTL_H_
+#include <sys/fcntl.h>
+#endif
+
+#endif
