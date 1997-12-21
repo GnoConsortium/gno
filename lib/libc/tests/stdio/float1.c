07177f48d15509c2277a71f7e52fe563839b6128
gdr <gdr>
1997-12-21 20:15:27 +0000

    test more floating point data formats in printf

--- lib/libc/tests/stdio/float1.c
+++ lib/libc/tests/stdio/float1.c
@@ -3,6 +3,10 @@
 int main(void) {
 	double pi = 3.141596;
 
-	printf("pi is %f\n", pi);
+	printf("(f) pi is %f\n", pi);
+	printf("(e) pi is %e\n", pi);
+	printf("(g) pi is %g\n", pi);
+	printf("(.2g) pi is %.2g\n", pi);
+	printf("(.2g) pi is %.2g\n", pi / 1024. );
 	return 0;
 }
