2a490753dc7127eee3b38b244ce021324ed2fd3d
gdr <gdr>
1997-12-21 19:57:19 +0000

    don't use FTS_WHITEOUT for GNO (yet)

--- include/fts.h
+++ include/fts.h
@@ -55,7 +55,9 @@ typedef struct {
 #define	FTS_PHYSICAL	0x010		/* physical walk */
 #define	FTS_SEEDOT	0x020		/* return dot and dot-dot */
 #define	FTS_XDEV	0x040		/* don't cross devices */
+#ifndef	__GNO__				/* (GNO isn't using whiteout yet) */
 #define	FTS_WHITEOUT	0x080		/* return whiteout information */
+#endif
 #define	FTS_OPTIONMASK	0x0ff		/* valid user option mask */
 
 #define	FTS_NAMEONLY	0x100		/* (private) child names only */
