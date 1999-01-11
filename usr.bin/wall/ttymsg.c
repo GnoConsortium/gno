2b00b1d41a3f19a0ac01f0f3d02dfc885eb732e3
gdr-ftp <gdr-ftp>
1999-01-11 01:24:47 +0000

    ttymsg.c: 	Corrected return type of writev(2).

--- usr.bin/wall/ttymsg.c
+++ usr.bin/wall/ttymsg.c
@@ -174,7 +174,7 @@ ttymsg(struct iovec *iov, int iovcnt, char *line, int tmout)
 }
 
 #ifdef __GNO__
-static size_t writev(int fd, struct iovec *iov, int iovcnt)
+ssize_t writev(int fd, struct iovec *iov, int iovcnt)
 {
 	int i;
 	size_t size, total_size = 0;
