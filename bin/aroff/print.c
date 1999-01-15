d1c781eaf43e6bcda56428457ef9ccceb2469865
gdr-ftp <gdr-ftp>
1999-01-15 08:36:31 +0000

    Makefile: 	- completely changed for new builds awgs.c, awgs.h: 	- renamed to aroff.c and aroff.h, respectively 	- inserted RCS Id tag 	- reformatted source (not substantive changes) aroff.1, aroff.rez, aroff.desc: 	- initial checkin

--- bin/aroff/print.c
+++ bin/aroff/print.c
@@ -3,6 +3,7 @@
 
     the code that formats each individual paragraph is here.
 
+    $Id: print.c,v 1.2 1999/01/15 08:36:31 gdr-ftp Exp $
 */
 
 #include <stdio.h>
@@ -39,72 +40,72 @@ char paraBuf[100][80];
    paragraph according to the ruler. */
 void printPara(RulerPtr ruler, pgraphPtr pptr)
 {
-char *txt;
-int width,z;
-int curLine, col, printcol, numctrl;
-int i,left,style;
-
-    curLine = col = printcol = 0;
-    txt = ((char *)pptr) + sizeof(pgraph);
-
+  char *txt;
+  int width,z;
+  int curLine, col, printcol, numctrl;
+  int i,left,style;
+  
+  curLine = col = printcol = 0;
+  txt = ((char *)pptr) + sizeof(pgraph);
+  
 calcLine:
     /* width determines how long this line is in characters; thus, where
        the break for word wrap will occur */
-    if (curLine == 0) width = (ruler->rightMargin - ruler->indentMargin)/8;
-    else width = (ruler->rightMargin - ruler->leftMargin)/8;
-    
-    while (*txt != 0x0d) {
-      switch (*txt) {
-          case 1: txt+=3; break;
-          case 2:
-                  style = *(++txt);
-                  if (noboldflag) { ++txt; break; }/* turn off boldfacing */
-                  if (style & 3) paraBuf[curLine][col++] = 15;
-                  else paraBuf[curLine][col++] = 14;
-                  txt++;
-                  break;
-          case 3: txt+=2; break;
-          case 4: txt+=2; break;
-          case 5:
-          case 6:
-          case 7: break;
-
-          default:
-            if (printcol == width) {
-              numctrl = 0;
-              for (z = col - 1; z > 0; z--) {
-                  if (paraBuf[curLine][z] == ' ') {
-                      if (z != col - 1)
-                          memcpy(&paraBuf[curLine+1][0],&paraBuf[curLine][z+1],
-                             (size_t) (col - z - 1));
-                      paraBuf[curLine][z] = 0;
-                      curLine++; printcol -= (z + 1 + numctrl);
-                      col -= (z + 1);
-                      goto calcLine;
-                  }
-                  else if (paraBuf[curLine][z] < ' ') numctrl++;
-              }
-              curLine++; col = printcol = 0;
-              /* one big word... don't break line */
-              goto calcLine;
-            }
-            paraBuf[curLine][col] = *(txt++);
-            printcol++; col++;
+  if (curLine == 0) width = (ruler->rightMargin - ruler->indentMargin)/8;
+  else width = (ruler->rightMargin - ruler->leftMargin)/8;
+  
+  while (*txt != 0x0d) {
+    switch (*txt) {
+    case 1: txt+=3; break;
+    case 2:
+      style = *(++txt);
+      if (noboldflag) { ++txt; break; }/* turn off boldfacing */
+      if (style & 3) paraBuf[curLine][col++] = 15;
+      else paraBuf[curLine][col++] = 14;
+      txt++;
+      break;
+    case 3: txt+=2; break;
+    case 4: txt+=2; break;
+    case 5:
+    case 6:
+    case 7: break;
+      
+    default:
+      if (printcol == width) {
+	numctrl = 0;
+	for (z = col - 1; z > 0; z--) {
+	  if (paraBuf[curLine][z] == ' ') {
+	    if (z != col - 1)
+	      memcpy(&paraBuf[curLine+1][0],&paraBuf[curLine][z+1],
+		     (size_t) (col - z - 1));
+	    paraBuf[curLine][z] = 0;
+	    curLine++; printcol -= (z + 1 + numctrl);
+	    col -= (z + 1);
+	    goto calcLine;
+	  }
+	  else if (paraBuf[curLine][z] < ' ') numctrl++;
+	}
+	curLine++; col = printcol = 0;
+	/* one big word... don't break line */
+	goto calcLine;
       }
+      paraBuf[curLine][col] = *(txt++);
+      printcol++; col++;
     }
-    paraBuf[curLine][col] = 0;
-    for (z = 0; z <= curLine; z++) {
-      if (z == 0) {
-          width = (ruler->rightMargin - ruler->indentMargin)/8;
-          left = (ruler->indentMargin)/8;
-      }
-      else {
-          width = (ruler->rightMargin - ruler->leftMargin)/8;
-          left = (ruler->leftMargin)/8;
-      }
-      for (i = 0; i < left; i++) putchar(' ');
-      printf("%s\n",paraBuf[z]);
+  }
+  paraBuf[curLine][col] = 0;
+  for (z = 0; z <= curLine; z++) {
+    if (z == 0) {
+      width = (ruler->rightMargin - ruler->indentMargin)/8;
+      left = (ruler->indentMargin)/8;
     }
+    else {
+      width = (ruler->rightMargin - ruler->leftMargin)/8;
+      left = (ruler->leftMargin)/8;
+    }
+    for (i = 0; i < left; i++) putchar(' ');
+    printf("%s\n",paraBuf[z]);
+  }
 }
 
 /* this is an obsolete routine that prints a paragraph with no
@@ -112,24 +113,24 @@ calcLine:
 #ifdef NOTDEFINED
 void printPara(RulerPtr ruler, pgraphPtr pptr)
 {
-char *txt;
-
-        txt = ((char *)pptr) + sizeof(pgraph);
-        while (*txt != 0x0D) {
-            switch (*txt) {
-                case 1: txt+=2; break;
-                case 2: txt++; break;
-                case 3: txt++; break;
-                case 4: txt++; break;
-                case 5:
-                case 6:
-                case 7: break;
-
-                default: putchar(*txt);
-            }
-            txt++;
-        }
-        putchar('\n');
+  char *txt;
+  
+  txt = ((char *)pptr) + sizeof(pgraph);
+  while (*txt != 0x0D) {
+    switch (*txt) {
+    case 1: txt+=2; break;
+    case 2: txt++; break;
+    case 3: txt++; break;
+    case 4: txt++; break;
+    case 5:
+    case 6:
+    case 7: break;
+      
+    default: putchar(*txt);
+    }
+    txt++;
+  }
+  putchar('\n');
 }
 #endif
 
@@ -138,20 +139,21 @@ char *txt;
 
 void printAWGS(void)
 {
-int z;
-pgraphPtr pptr;
-char *txt;
-char x;
-
-    for (z = 0; z < docSACount; z++) {
-        pptr = (pgraphPtr) (((byte *)docTextBlocks[docSaveArray[z].textBlock])
-               + docSaveArray[z].offset);
+  int z;
+  pgraphPtr pptr;
+  char *txt;
+  char x;
+  
+  for (z = 0; z < docSACount; z++) {
+    pptr = (pgraphPtr) (((byte *)docTextBlocks[docSaveArray[z].textBlock])
+			+ docSaveArray[z].offset);
     
 #ifdef DEBUG
-fprintf(stderr,"[%d] offset %d  paragraph : %08lX",z,docSaveArray[z].offset,
-  pptr);
-fprintf(stderr," textBlock: %08lX\n",docTextBlocks[docSaveArray[z].textBlock]);
+    fprintf(stderr, "[%d] offset %d  paragraph : %08lX", z,
+	    docSaveArray[z].offset, pptr);
+    fprintf(stderr, " textBlock: %08lX\n",
+	    docTextBlocks[docSaveArray[z].textBlock]);
 #endif
-        printPara(&docRulers[docSaveArray[z].rulerNum],pptr);
-    }
+    printPara(&docRulers[docSaveArray[z].rulerNum],pptr);
+  }
 }
